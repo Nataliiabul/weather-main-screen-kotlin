21f55d12905265408efde1e6f4a5a1dd66d92c3a
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-07-13 20:51:22 +0300

    added design

--- app/src/main/java/com/example/weathermainscreen/MainActivity.kt
+++ app/src/main/java/com/example/weathermainscreen/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.weathermainscreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showCustomUI()
+    }
+
+    private fun showCustomUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 }
